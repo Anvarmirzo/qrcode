073cba376f5d00dea9bfde73811c81b86c44bbb2
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2021-12-31 10:49:36 +0500

    finished

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // https://goqr.me/api/doc/create-qr-code
-(async () => {
-	const imgUrl = await fetch(
-		' https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example',
-	);
+document.addEventListener('DOMContentLoaded', function (event) {
+	const form = document.getElementById('form');
 
-	const img = document.getElementById('qrcode');
-	img.src = imgUrl.url;
-})();
+	form.addEventListener('submit', async (e) => {
+		e.preventDefault();
+
+		const request = form.querySelector('input').value;
+		const img = document.getElementById('qrcode');
+		const downloadLink = document.getElementById('download-link');
+
+		const imgUrl = await fetch(
+			`https://api.qrserver.com/v1/create-qr-code/?&data=${request}`,
+		);
+
+		downloadLink.href = imgUrl.url;
+		downloadLink.title = imgUrl.url;
+		img.src = imgUrl.url;
+	});
+});
